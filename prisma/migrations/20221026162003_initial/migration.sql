16c79d5db83ebc8f4c573cc1eea009e08eec11ba
daichi.suyama <symdit@gmail.com>
2022-11-04 16:07:38 +0900

    reset cache

--- prisma/migrations/20221026162003_initial/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "user" (
-    "id" SERIAL NOT NULL,
-    "name" TEXT NOT NULL,
-    "created_at" TIMESTAMP(3) NOT NULL,
-
-    CONSTRAINT "user_pkey" PRIMARY KEY ("id")
-);
