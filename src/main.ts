ee914514e3f66fc750ae8536d2886c521fbd4be6
daichi.suyama <symdit@gmail.com>
2022-11-24 23:18:04 +0900

    feat: enable `timeout.interceptor`

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
+import { TimeoutInterceptor } from './common/interceptors/timeout.interceptor';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalInterceptors(new TimeoutInterceptor());
   await app.listen(3000);
 }
 bootstrap();
