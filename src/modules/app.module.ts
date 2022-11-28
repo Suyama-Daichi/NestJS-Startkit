4fbd280c47515d713bb1d27a906f601632a83197
daichi.suyama <symdit@gmail.com>
2022-11-29 01:16:56 +0900

    wip: install `@nestjs/config`

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -2,10 +2,12 @@ import { AppController } from '@controllers/app.controller';
 import { Module } from '@nestjs/common';
 import { AppService } from '@services/app.service';
 import { PrismaService } from '@services/prisma.service';
-import { AuthService } from './services/auth.service';
-import { AuthController } from './controllers/auth.controller';
+import { AuthService } from '../services/auth.service';
+import { AuthController } from '../controllers/auth.controller';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
+  imports: [ConfigModule.forRoot()],
   controllers: [AppController, AuthController],
   providers: [AppService, PrismaService, AuthService],
 })
