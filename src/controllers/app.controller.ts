a57b24b6be3df7a27d908eff38cf742d2dd958f4
daichi.suyama <symdit@gmail.com>
2022-11-30 00:53:54 +0900

    feat: create `JwtAuthGuard`

--- src/controllers/app.controller.ts
+++ src/controllers/app.controller.ts
@@ -1,10 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
+import { JwtAuthGuard } from '@/modules/auth/jwt-auth.guard';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { AppService } from '@services/app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @UseGuards(JwtAuthGuard)
   @Get()
   getHello() {
     return this.appService.getHello();
