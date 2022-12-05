ec1099c4f7a00afea8a5d488039ffa317e626bd8
daichi.suyama <symdit@gmail.com>
2022-12-05 19:16:59 +0900

    config: enable CORS

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { AppModule } from './modules/app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalInterceptors(new TimeoutInterceptor());
+  app.enableCors({ origin: ['http://localhost:3000'] });
   await app.listen(3000);
 }
 bootstrap();
